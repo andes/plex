07421c73d3231ed358d45368057faf5b3ef2d517
jfgabriel <jgabriel@hospitalneuquen.org.ar>
2016-08-24 08:27:19 -0300

    mdi icons

--- webpack.entry.js
+++ webpack.entry.js
@@ -1,3 +1,4 @@
+require('./node_modules/mdi/css/materialdesignicons.css');
 require('./less/montserrat/TypoPRO-Montserrat.css');
 require('./node_modules/bootstrap/dist/css/bootstrap.css');
 require('./less/adminLTE/build.less');
