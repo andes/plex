7d5058cfdac77ddd93087c46dd4b4325f0840811
hugo <hfernandez@neuquen.gov.ar>
2016-10-18 08:24:44 -0300

    separo html validation messages

--- src/lib/validation-messages/validation-messages.component.ts
+++ src/lib/validation-messages/validation-messages.component.ts
@@ -2,23 +2,9 @@ import { Component, Input } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
 @Component({
+    moduleId: module.id,
     selector: 'plex-validation-messages',
-    template: `<div class="help-block" *ngIf="control.errors && control.errors.required">
-                    <span>Valor requerido</span>
-               </div>
-               <div class="help-block" *ngIf="control.errors && control.errors.min">
-                    <span>Valor no permitido</span>
-               </div>
-               <div class="help-block" *ngIf="control.errors && control.errors.max">
-                    <span>Valor no permitido</span>
-               </div>
-               <div class="help-block" *ngIf="control.errors && control.errors.minlength">
-                    <span>Debe ingresar un mínimo de {{control.errors.minlength.requiredLength}} caracteres</span>
-               </div>
-               <div class="help-block" *ngIf="control.errors && control.errors.format">
-                    <span>Formato incorrecto</span>
-               </div>
-               `
+    templateUrl: 'validation-messages.html', 
 })
 export class ValidationMessagesComponent {
     @Input() control: FormControl;
