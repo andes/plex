ed59acc1af3dfb531897cf2d9a36b34317aa2ce9
Juan G <Juan G>
2016-10-23 20:51:38 -0300

    selectize

--- src/demo/app/select/select.component.ts
+++ src/demo/app/select/select.component.ts
@@ -9,44 +9,46 @@ import {
 } from '@angular/forms';
 
 //Importo un servicio de prueba
-import { 
-    ServiceDemoSelect 
+import {
+    ServiceDemoSelect
 } from './serviceDemoSelect';
 
 @Component({
     templateUrl: 'select.html',
 })
 export class SelectDemoComponent {
-    
+
     public selectableItems: any[];
-    public miModelo: any;
-    
-    
-    constructor(private formBuilder: FormBuilder, private serviceDemoSelect: ServiceDemoSelect) {}
+    public modelo: any;
+
+
+    constructor(private formBuilder: FormBuilder, private serviceDemoSelect: ServiceDemoSelect) { }
 
     ngOnInit() {
 
         //Inicializo el modelo en null la primera vez
         //this.selectedItem = "";
 
-        this.serviceDemoSelect.get().subscribe(resultado => {this.selectableItems = resultado; this.miModelo = resultado[0]});
+        // this.serviceDemoSelect.get().subscribe(resultado => {this.selectableItems = resultado; this.miModelo = resultado[0]});
 
-        //Prueba de datos simples
-        // this.selectableItems = [{
-        //     nombre: 'valor del primer item',
-        //     valor: 1
-        // }, {
-        //     nombre: 'valor del segundo item',
-        //     valor: 2
-        // },
-        // {
-        //     nombre: 'valor del tercer item',
-        //     valor: 3
-        // }];
+        this.modelo = {
+            select: [{
+                nombre: 'valor del segundo item',
+                id: 2
+            }]
+        }
 
-
-        
-            
+        // Prueba de datos simples
+        this.selectableItems = [{
+            nombre: 'valor del primer item',
+            id: 1
+        }, {
+            nombre: 'valor del segundo item',
+            id: 2
+        },
+        {
+            nombre: 'valor del tercer item',
+            id: 3
+        }];
     }
-
 }
--- src/lib/selectize/select.component.ts
+++ src/lib/selectize/select.component.ts
@@ -14,37 +14,89 @@ import { ControlValueAccessor, FormControl, NgControl, NG_VALUE_ACCESSOR } from 
     ]
 })
 export class PlexSelectComponent implements OnInit {
-    private renderer: Renderer;
     private onChange = (_: any) => { };
-    @ViewChild('ref') ref: ElementRef;
     @ContentChild(NgControl) control: any;
 
     // Input properties
     @Input('auto-focus') autofocus: boolean;
     @Input() label: string;
     @Input() placeholder: string;
+    @Input() multiple: false;
+    @Input('id-field') idField: string;
+    @Input('label-field') labelField: string;
+    @Input('group-field') groupField: string;
+    @Input() data: any[];
 
-    constructor(renderer: Renderer) {
-        this.renderer = renderer;
+    constructor(private element: ElementRef, private renderer: Renderer) {
         this.placeholder = "";
+        this.multiple = false;
+        this.idField = "id";
+        this.labelField = "nombre";
+        this.groupField = "grupo";
     }
 
     // Inicialización
     ngOnInit() { }
     ngAfterViewInit() {
-        $('#select-beast').selectize({
-            sortField: 'text'
+        let self = this;
+        $(this.element.nativeElement.children[0]).selectize({
+            valueField: this.idField,
+            labelField: this.labelField,
+            placeholder: this.placeholder,
+            options: this.data,
+            onChange: function (value) {
+                // Busca en la lista de items un valor que coincida con la clave
+                if (self.multiple) {
+                    let result = [];
+                    for (let i = 0; i < self.data.length; i++) {
+                        if (value.indexOf("" + self.data[i][self.idField]) >= 0) { // value es siempre un string, por eso es necesario convertir el id
+                            result = [...result, self.data[i]];
+                        }
+                    }
+                    self.onChange(result.length ? result : null);
+                }
+                else {
+                    for (let i = 0; i < self.data.length; i++) {
+                        if ("" + self.data[i][self.idField] == value) { // value es siempre un string, por eso es necesario convertir el id
+                            self.onChange(self.data[i]);
+                            return;
+                        }
+                    }
+                    self.onChange(null);
+                }
+            }
         });
     }
 
     // Actualización Modelo -> Vista
     writeValue(value: any) {
-        this.renderer.setElementProperty(this.ref.nativeElement, 'value', value);
+        let self = this;
+        var valueAsString = function (val: any): string {
+            if (val == null)
+                return null;
+            else
+                if (typeof val == "object")
+                    return "" + val[self.idField];
+                else
+                    return "" + val;
+        }
+
+        // Busca el id que corresponde al item
+        let val;
+        if (Array.isArray(value)) {
+            val = [];
+            for (let i = 0; i < value.length; i++) {
+                val = [...val, valueAsString(value[i])];
+            }
+        } else {
+            val = valueAsString(value);
+        }
+        
+        this.element.nativeElement.children[0].selectize.setValue(val, true);
     }
 
     // Actualización Vista -> Modelo
     registerOnTouched() {
-
     }
     registerOnChange(fn: any) {
         this.onChange = function (value) {
