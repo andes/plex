9ab99721019d450fb0ff58ac3a48671fdfea3749
Juan G <Juan G>
2017-03-21 07:43:54 -0300

    mejoras a plex-box

--- src/lib/box/box.component.ts
+++ src/lib/box/box.component.ts
@@ -5,7 +5,6 @@ import { Component, Input } from '@angular/core';
   templateUrl: 'box.html'
 })
 export class PlexBoxComponent {
-  @Input() title: string;
   @Input() type = '';
 
   constructor() {
