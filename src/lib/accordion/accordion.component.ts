62f77ce018879583a8caca4975673dccca73fa58
Daniela <andresin@gmail.com>
2017-04-06 08:43:04 -0300

    plex-accordion bootstrap4 paneles collapsed opcional

--- src/lib/accordion/accordion.component.ts
+++ src/lib/accordion/accordion.component.ts
@@ -5,19 +5,10 @@ import { PlexPanelComponent } from './panel.component';
     selector: 'plex-accordion',
     templateUrl: 'accordion.html'
 })
-export class PlexAccordionComponent implements AfterViewInit {
+export class PlexAccordionComponent {
     public panels: PlexPanelComponent[] = [];
 
-    ngAfterViewInit() {
-        if (this.panels.length) {
-            this.panels[0].active = true;
-        }
-    }
-
     addPanel(panel: PlexPanelComponent) {
-        if (this.panels.length === 0) {
-            panel.active = true;
-        }
         this.panels.push(panel);
     }
 }
--- src/lib/accordion/panel.component.ts
+++ src/lib/accordion/panel.component.ts
@@ -2,22 +2,22 @@ import { Component, Input } from '@angular/core';
 import { PlexAccordionComponent } from './accordion.component';
 
 @Component({
-  selector: 'plex-panel',
-   templateUrl: 'panel.html'
+    selector: 'plex-panel',
+    templateUrl: 'panel.html'
 })
 export class PlexPanelComponent {
-  @Input() tituloPrincipal: string;
-  @Input() tituloSecundario: string;
-  @Input() icon: string;
-  @Input() content: string;
-  @Input() active: boolean;
+    @Input() tituloPrincipal: string;
+    @Input() tituloSecundario: string;
+    @Input() icon: string;
+    @Input() content: string;
+    @Input() active: boolean;
 
-  constructor(accordion: PlexAccordionComponent) {
-    accordion.addPanel(this);
-  }
+    constructor(accordion: PlexAccordionComponent) {
+        accordion.addPanel(this);
+    }
 
-  selectPanel(panel: PlexPanelComponent) {
-      this.active = !this.active;
-  }
+    selectPanel(panel: PlexPanelComponent) {
+        this.active = !this.active;
+    }
 
 }
