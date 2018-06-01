9fcdac3a0419f18125306327d036679da6248119
Mariano Botta <marianoabotta@gmail.com>
2018-06-01 11:17:35 -0300

    select plex-button in footer

--- src/lib/layout/footer.component.ts
+++ src/lib/layout/footer.component.ts
@@ -5,10 +5,10 @@ import { Component, Input } from '@angular/core';
   template: `
     <div class="row">
         <div class="col">
-            <ng-content select="plex-button[left]"></ng-content>
+            <ng-content select="plex-button[position=left]"></ng-content>
         </div>
         <div class="col text-right">
-            <ng-content select="plex-button[right]"></ng-content>
+            <ng-content select="plex-button[position=right]"></ng-content>
         </div>
     </div>
   `,
