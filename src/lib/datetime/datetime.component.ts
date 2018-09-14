7ce0a77b3841a0dc19dd07e9d0b0817ae40aaa0e
Andrés <andresin@gmail.com>
2018-09-14 10:58:15 -0300

    Se actualiza demo con date por type y con min/max

--- src/lib/datetime/datetime.component.ts
+++ src/lib/datetime/datetime.component.ts
@@ -192,11 +192,13 @@ export class PlexDateTimeComponent implements OnInit, AfterViewInit, OnChanges {
     prev() {
         let temp = this.value ? moment(this.value, this.dateOrTime()).subtract(1, this.skipBy).format(this.format) : null;
         this.setElements(temp);
+        this.onChange(this.value);
     }
 
     next() {
         let temp = this.value ? moment(this.value, this.dateOrTime()).add(1, this.skipBy).format(this.format) : null;
         this.setElements(temp);
+        this.onChange(this.value);
     }
 
     private setElements(temp: string) {
