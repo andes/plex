4f14b3c4d608017b5d3ea75e5d228dd5434a29fc
Mariano Botta <marianoabotta@gmail.com>
2018-10-31 14:49:06 -0300

    fix: corrige 'Invalid Date' en plex-datetime

--- src/lib/datetime/datetime.component.ts
+++ src/lib/datetime/datetime.component.ts
@@ -156,18 +156,21 @@ export class PlexDateTimeComponent implements OnInit, AfterViewInit, OnChanges {
     registerOnTouched() {
     }
 
+    parseDate(value) {
+        if (typeof value === 'string') {
+            const m = moment(value, this.format);
+            if (m.isValid()) {
+                value = m.toDate();
+            } else {
+                value = null;
+            }
+        }
+        return value;
+    }
+
     registerOnChange(fn: any) {
         this.onChange = (value) => {
-            if (typeof value === 'string') {
-                const m = moment(value, this.format);
-                if (m.isValid()) {
-                    value = m.toDate();
-                } else {
-                    value = null;
-                }
-            }
-
-            this.value = value;
+            this.value = this.parseDate(value);
             fn(value);
             this.change.emit({
                 value
@@ -176,6 +179,7 @@ export class PlexDateTimeComponent implements OnInit, AfterViewInit, OnChanges {
     }
 
     onBlur() {
+        this.value = this.parseDate(this.value);
         this.writeValue(this.value);
     }
 
