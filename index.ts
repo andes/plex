17fdfceed2b59d2fb5ed426d177f01f969d152dd
Mariano Andres Botta <marianoabotta@gmail.com>
2018-12-14 14:08:53 -0300

    add: unsubscriber decorator

--- index.ts
+++ index.ts
@@ -2,3 +2,4 @@ export * from './src/lib/module';
 export * from './src/lib/core/service';
 export * from './src/lib/dropdown/dropdown-item.inteface';
 export * from './src/lib/select/select-event.interface';
+export * from './src/lib/decorators';
