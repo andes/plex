dbe06cb56f313265e22324cf9e782a39180e3672
Andrés <andresin@gmail.com>
2019-07-04 11:18:37 -0300

    Opciones para visualización de badges/botones

--- src/lib/item-list/item.component.ts
+++ src/lib/item-list/item.component.ts
@@ -10,6 +10,8 @@ export class PlexItemComponent implements OnInit {
     @Input() layout: 'completo' | 'contenido' | 'izquierda' | 'derecha' = 'completo';
     @Input() headings: any = {};
     @Input() striped = false;
+    @Input() botonera = true;
+    @Input() badges = true;
 
     ngOnInit() {
         this.layout = this.layout ? this.layout : 'completo';
