8bfb2e73aad6538b05225283efdaefb6c8bfd302
Mariano Andres Botta <marianoabotta@gmail.com>
2019-08-22 11:57:49 -0300

    fix: remove templateurls

--- src/lib/label/label.component.ts
+++ src/lib/label/label.component.ts
@@ -2,10 +2,13 @@ import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'plex-label',
-    templateUrl: 'label.html',
-    styleUrls: [
-        'plex-label.scss'
-    ]
+    template: `
+        <div class="plex-label d-flex flex-column" [ngClass]="size">
+            <span *ngIf="titulo" [ngClass]="{'font-weight-bold': tituloBold}">{{ titulo }}</span>
+            <small *ngIf="titulo && subtitulo">{{ subtitulo }}</small>
+            <ng-content *ngIf="!titulo && !subtitulo"></ng-content>
+        </div>
+    `
 })
 export class PlexLabelComponent {
     @Input() titulo: string;
