319e66ff710ffad71b0239d79fd16f9381b93a55
Andrés Boc-Ho <andresin@gmail.com>
2019-10-11 15:56:40 -0300

    feature: panel de informacion general

--- src/lib/help/help.component.ts
+++ src/lib/help/help.component.ts
@@ -3,31 +3,43 @@ import { Component, Input } from '@angular/core';
 @Component({
     selector: 'plex-help',
     template: `
-    <div class="card" [ngClass]="{'open': !closed}">
-        <plex-button *ngIf="!closed" type="danger" size="sm" icon="close" (click)="toggleClose()"></plex-button>
-        <plex-button *ngIf="closed && !tituloBoton" type="info" size="sm" [icon]="icon" (click)="toggleClose()"></plex-button>
-        <plex-button *ngIf="closed && tituloBoton" type="info" size="sm" [label]="tituloBoton" (click)="toggleClose()"></plex-button>
+    <div class="toggle-{{ type }}" [ngClass]="{'closed': closed}">
+        <plex-button *ngIf="!closed" type="danger" size="sm" icon="close" (click)="toggleOpenClose()"></plex-button>
+        <plex-button *ngIf="closed && !tituloBoton" type="info" size="sm" [icon]="icon" (click)="toggleOpenClose()"></plex-button>
+        <plex-button *ngIf="closed && tituloBoton" type="info" size="sm" [label]="tituloBoton" (click)="toggleOpenClose()"></plex-button>
+    </div>
+    <div class="card {{ type }}" [ngClass]="{'open': !closed}" *ngIf="type === 'help'">
         <ng-container *ngIf="!closed">
             <div class="card-header">
                 <plex-title [titulo]="titulo" size="sm">
                 </plex-title>
-            </div>
-            <div class="card-body">
+                </div>
+                <div class="card-body">
                 <ng-content></ng-content>
             </div>
         </ng-container>
     </div>
+    <ng-container *ngIf="!closed && type === 'info'">
+        <div class="jumbotron {{ type }}" [ngClass]="{'open': !closed}">
+            <h1 class="display-6">{{ titulo }}</h1>
+            <p class="lead" *ngIf="subtitulo"><b>{{ subtitulo }}</b></p>
+            <ng-content select="[info]"></ng-content>
+        </div>
+    </ng-container>
     `
 })
 export class PlexHelpComponent {
+    @Input() type: 'info' | 'help' = 'info';
     @Input() titulo = '';
+    @Input() subtitulo: string;
     @Input() tituloBoton = '';
-    @Input() icon = 'help';
+    @Input() icon = 'help-circle';
+    unlisten: Function;
     closed = true;
 
     constructor() { }
 
-    toggleClose() {
+    public toggleOpenClose() {
         this.closed = !this.closed;
     }
 }
