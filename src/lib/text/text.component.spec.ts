e7044082725b8a46492e523c0b2ba64dffd8d569
Mariano Andres Botta <marianoabotta@gmail.com>
2019-11-14 19:27:14 -0300

    test(plex-text): add first unit test

---
+++ src/lib/text/text.component.spec.ts
@@ -0,0 +1,169 @@
+
+import { async, ComponentFixture, discardPeriodicTasks, fakeAsync, flush, flushMicrotasks, TestBed, tick } from '@angular/core/testing';
+import { By, BrowserModule } from '@angular/platform-browser';
+import { Component, DebugElement, ErrorHandler, NgZone, Type, ViewChild, ViewEncapsulation } from '@angular/core';
+// import { ConsoleService } from './console.service';
+// import { FormsModule } from '@angular/forms';
+// import { getNgSelectElement, selectOption, TestsErrorHandler, tickAndDetectChanges, triggerKeyDownEvent } from '../testing/helpers';
+// import { MockConsole, MockNgZone } from '../testing/mocks';
+
+import { PlexTextComponent } from './text.component';
+import { PlexModule } from '../module';
+import { Subject } from 'rxjs';
+import { FormsModule } from '@angular/forms';
+
+describe('PlexTextComponent', () => {
+    describe('input simple text', () => {
+        let fixture: ComponentFixture<PlexTextTestComponent>;
+        let text: PlexTextComponent;
+        let input: HTMLInputElement;
+        let nativeText: HTMLInputElement;
+
+        beforeEach(fakeAsync(() => {
+            fixture = createTestingModule(
+                PlexTextTestComponent,
+                `<plex-text [(ngModel)]="text" (change)="onChange($event)">
+                </plex-text>`);
+            text = fixture.componentInstance.plexText;
+            input = getElement(fixture, 'plex-text input').nativeElement;
+            nativeText = getElement(fixture, 'plex-text').nativeElement;
+        }));
+
+        it('writing must update Input variable', fakeAsync(() => {
+            tickAndDetectChanges(fixture);
+            writePlexText(nativeText, 'hola');
+            tickAndDetectChanges(fixture);
+            expect(fixture.componentInstance.text).toBe('hola');
+        }));
+
+        it('changing input bar must update html DOM', fakeAsync(() => {
+            tickAndDetectChanges(fixture);
+            fixture.componentInstance.text = 'hola';
+            tickAndDetectChanges(fixture);
+            expect(input.value).toBe('hola');
+        }));
+
+        it('after write change must be called', fakeAsync(() => {
+            spyOn(fixture.componentInstance, 'onChange');
+            writePlexText(input, 'hola');
+            tickAndDetectChanges(fixture);
+            expect(fixture.componentInstance.onChange).toHaveBeenCalledWith({ value: 'hola' });
+        }));
+    });
+});
+
+function writePlexText(element, text) {
+    element.value = text;
+    element.dispatchEvent(new Event('input'));
+}
+
+export function tickAndDetectChanges(fixture: ComponentFixture<any>) {
+    fixture.detectChanges();
+    tick();
+}
+
+export function getPlexTextElement(fixture: ComponentFixture<any>): DebugElement {
+    return fixture.debugElement.query(By.css('plex-text'));
+}
+
+export function getElement(fixture: ComponentFixture<any>, selector: string): DebugElement {
+    return fixture.debugElement.query(By.css(selector));
+}
+
+export function triggerKeyEvent(event: string, element: DebugElement, which: number, key = ''): void {
+    element.triggerEventHandler(event, {
+        which,
+        key,
+        preventDefault: () => { },
+    });
+}
+
+@Component({
+    template: ``
+})
+class PlexTextTestComponent {
+    @ViewChild(PlexTextComponent, { static: false }) plexText: PlexTextComponent;
+    text = 'hola';
+
+    onChange(_: Event) {
+    }
+
+    onFocus(_: Event) {
+    }
+
+    onBlur(_: Event) {
+    }
+
+    onOpen() {
+    }
+
+    onClose() {
+    }
+
+    onAdd() {
+    }
+
+    onRemove() {
+    }
+
+    onClear() {
+    }
+
+    onSearch() {
+    }
+
+    onScroll() {
+    }
+
+    onScrollToEnd() {
+    }
+}
+
+@Component({
+    template: ``,
+    encapsulation: ViewEncapsulation.Native,
+})
+class EncapsulatedPlexTextTestComponent extends PlexTextTestComponent {
+    @ViewChild(PlexTextComponent, { static: true }) select: PlexTextComponent;
+}
+
+function createTestingModule<T>(cmp: Type<T>, template: string): ComponentFixture<T> {
+
+    TestBed.configureTestingModule({
+        imports: [BrowserModule, PlexModule, FormsModule],
+        declarations: [cmp],
+        // providers: [
+        // { provide: ErrorHandler, useClass: TestsErrorHandler },
+        // { provide: NgZone, useFactory: () => new MockNgZone() },
+        // { provide: ConsoleService, useFactory: () => new MockConsole() }
+        // ]
+    })
+        .overrideComponent(cmp, {
+            set: {
+                template
+            }
+        });
+
+
+    TestBed.compileComponents();
+
+    const fixture = TestBed.createComponent(cmp);
+    fixture.detectChanges();
+    return fixture;
+}
+
+function createEvent(target = {}) {
+    return {
+        preventDefault: () => {
+        },
+        target: {
+            className: '',
+            tagName: '',
+            classList: {
+                contains: () => {
+                }
+            },
+            ...target
+        }
+    };
+}
