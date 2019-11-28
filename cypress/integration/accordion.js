afa19b9fd22319f00c64a0d56edf72eeae8776a1
Mariano Andres Botta <marianoabotta@gmail.com>
2019-11-28 13:27:42 -0300

    chore(applitool): set up

--- cypress/integration/accordion.js
+++ cypress/integration/accordion.js
@@ -2,10 +2,12 @@
 
 context('accordion', () => {
     before(() => {
+        cy.eyesOpen({ appName: 'PLEX', testName: 'accordion' });
         cy.visit('/accordion');
     });
 
     it('test accordion', () => {
+        cy.eyesCheckWindow('main - accordion');
         cy.plexAccordion().plexPanel(0).as('primerPanel');
         cy.get('@primerPanel').contains('TOOGLE INACTIVE');
         cy.get('@primerPanel').plexBool('name="activo"', true);
@@ -13,5 +15,8 @@ context('accordion', () => {
         cy.plexAccordion(0);
         cy.plexAccordion(1);
         cy.plexAccordion().plexPanel(1).contains('SEGUNDO PANEL');
+        cy.eyesCheckWindow('end - accordion');
+        cy.eyesClose();
+
     });
 });
