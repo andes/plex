ad2cbcd69fd668832a98e3edb89d95b764724ccd
Mariano Andres Botta <marianoabotta@gmail.com>
2019-12-16 14:42:27 -0300

    feat(plex-badge): add color attribute

--- src/lib/badge/badge.component.ts
+++ src/lib/badge/badge.component.ts
@@ -1,18 +1,29 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ElementRef, ViewChild, OnChanges } from '@angular/core';
 
 @Component({
     selector: 'plex-badge',
     template: `
-        <span class="badge badge-{{type}} badge-{{size}}">
+        <span #badge class="badge badge-{{type}} badge-{{size}}">
             <ng-content></ng-content>
         </span>
                 `,
 })
-export class PlexBadgeComponent {
+export class PlexBadgeComponent implements OnChanges {
     @Input() type: string;
     @Input() size: 'xs' | 'sm' | 'md' | 'lg' | 'xl' = 'md';
+    @Input() color: string;
+
+    @ViewChild('badge', { static: true }) el: ElementRef;
 
     constructor() {
         this.type = 'success';
     }
+
+    ngOnChanges() {
+        if (this.color && this.color.length > 0) {
+            this.el.nativeElement.style.setProperty('--badge-color', this.color);
+        }
+
+    }
+
 }
