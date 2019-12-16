24d3d9ff2b7a3f4b59d2870f0f49e0332041a550
Mariano Andres Botta <marianoabotta@gmail.com>
2019-12-16 13:16:13 -0300

    feat(plex-icon): add prefix attribute

--- src/lib/icon/icon.component.ts
+++ src/lib/icon/icon.component.ts
@@ -1,11 +1,11 @@
-import { NgForm } from '@angular/forms';
-import { Component, Input, HostBinding, HostListener, Optional, forwardRef } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'plex-icon',
-    template: `<i class="mdi mdi-{{name}} text-{{type}}"></i> `,
+    template: `<i class="{{prefix}} {{prefix}}-{{name}} text-{{type}}"></i> `,
 })
 export class PlexIconComponent {
+    @Input() prefix = 'mdi';
     @Input() name: string;
     @Input() type: 'success' | 'info' | 'warning' | 'danger' | 'primary';
 
