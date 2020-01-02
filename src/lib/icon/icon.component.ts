e1abf74235ebdc973745e5e48f9b19e3935523c3
Mariano Andres Botta <marianoabotta@gmail.com>
2020-01-02 15:25:55 -0300

    feat(plex-icon): add size propertie

--- src/lib/icon/icon.component.ts
+++ src/lib/icon/icon.component.ts
@@ -2,15 +2,23 @@ import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'plex-icon',
-    template: `<i class="{{prefix}} {{prefix}}-{{name}} text-{{type}}"></i> `,
+    template: `<i class="{{prefix}} {{prefix}}-{{name}} text-{{type}} {{ cssSize }}"></i> `,
 })
 export class PlexIconComponent {
     @Input() prefix = 'mdi';
     @Input() name: string;
     @Input() type: 'success' | 'info' | 'warning' | 'danger' | 'primary';
+    @Input() size: '18' | '24' | '36' | '48';
 
     constructor() {
 
+    }
+
+    get cssSize() {
+        if (this.size) {
+            return `${this.prefix}-${this.size}px`;
+        }
+        return '';
     }
 
 }
