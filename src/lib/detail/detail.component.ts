ff38c48c295007930709398ae121bec8db80eb8a
Mariano Andres Botta <marianoabotta@gmail.com>
2020-01-02 13:16:17 -0300

    feat(plex-detail): add plex-detail

--- src/lib/detail/detail.component.ts
+++ src/lib/detail/detail.component.ts
@@ -3,33 +3,38 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
     selector: 'plex-detail',
     template: `
-        <section [ngClass]="{'vertical': horizontalidad}">
-            <div>
+        <section [ngClass]="cssDirection" class="size-{{size}}">
+            <div class="contenedor-elementos-graficos">
                 <ng-content select="plex-icon"></ng-content>
-                <img *ngIf="foto" class="rounded-circle cover" src="{{ foto }}" alt="">    
+                <ng-content select="img"></ng-content>
+                <!-- <img *ngIf="foto" class="rounded-circle cover" src="{{ foto }}" alt=""> -->
             </div>
-            <div>
+            <div class="contenedor-textos" [ngClass]="{ 'd-flex flex-column': direction === 'column'  }">
                 <ng-content select="plex-badge"></ng-content>
-                <h3 *ngIf="titulo">{{ titulo }}</h3>
-                <h5 *ngIf="subtitulo">{{ subtitulo }}</h5>
-                <hr class="w-25 float-left">
-                <ng-content select="plex-label"></ng-content>
+                <ng-content select="div[title]"></ng-content>
+                <ng-content select="div[subtitle]"></ng-content>
+                <hr>
             </div>
         </section>
+
+        <section class="contenedor-datos-secundarios">
+            <ng-container *ngFor="let dato of datos">
+                <plex-label titulo="{{ dato.label }}" subtitulo="{{ dato.valor }}"></plex-label>
+            </ng-container>
+            <ng-content select="plex-label"></ng-content>
+        </section>
+
     `,
 })
 
-export class PlexDetailComponent implements OnInit {
+export class PlexDetailComponent {
+    @Input() direction: 'column' | 'row' = 'row';
+    @Input() size: 'xs' | 'md' | 'lg' = 'md';
+    @Input() datos: [];
 
-
-    @Input() horizontalidad: boolean;
-    @Input() foto: string;
-    @Input() icon: string;
-    @Input() titulo: string;
-    @Input() subtitulo: string;
-    @Input() datoSecundario: string;
-
-    ngOnInit() {
+    get cssDirection() {
+        return this.direction === 'row' ? 'direction-row' : 'direction-column';
     }
+
 
 }
