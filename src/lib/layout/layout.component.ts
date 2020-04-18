57d813e9dcea67a982c6e57cc7126e47c2361938
Mariano Andres Botta <marianoabotta@gmail.com>
2020-04-18 13:18:01 -0300

    feat(layout-responsive): responsive layout

--- src/lib/layout/layout.component.ts
+++ src/lib/layout/layout.component.ts
@@ -1,14 +1,31 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ContentChild, AfterViewInit, AfterContentInit, AfterContentChecked, ComponentRef, ElementRef } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { EventEmitter } from 'events';
+
+/**
+ * ATENCION! SI NO SE ESTABLECE FOCO SE MANTIENE FUNCIONALIDAD ANTERIOR.
+ *
+ * TIENEN UN NGIF SI MAIN = 12
+ * HACIENDO QUE EL CONTENIDO DEL SIDEBAR NO SE RENDERIZE.
+ * SI SIMPLEMENTE LO OCULTO, PUEDE CAUSAR ERRORES QUE NO SABEMOS
+ * NO TODOS LOS SIDEBAR DEBEN ESTAR PREPARADOS
+ */
 
 @Component({
     selector: 'plex-layout',
     template: `
-    <section>
+    <section responsive>
         <div class="row">
-            <div class="col-{{ main }} h-100">
+            <div class="col-{{ main }} h-100"
+                 [class.focused]="foco === 'main'"
+                 [class.not-focused]="foco !== 'main'">
                 <ng-content select="plex-layout-main"></ng-content>
             </div>
-            <div class="col-{{ maxcolumns - main }} h-100" *ngIf="main < maxcolumns">
+            <div class="col-{{ maxcolumns - main }} h-100"
+                 *ngIf="(main < maxcolumns && !foco) || !!foco"
+                 [hidden]="main === maxcolumns && foco"
+                 [class.focused]="foco === 'sidebar'"
+                 [class.not-focused]="foco !== 'sidebar'">
                 <ng-content select="plex-layout-sidebar"></ng-content>
             </div>
         </div>
@@ -17,9 +34,15 @@ import { Component, Input } from '@angular/core';
   `,
 })
 export class PlexLayoutComponent {
+    // Compatibility mode
     public maxcolumns = 12;
     @Input() main = 12;
 
+    @Input() foco: 'main' | 'sidebar' = null;
+
+
     constructor() {
     }
+
+
 }
