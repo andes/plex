091a99c9f630044f457b90de334aace5f122cfeb
Mariano Andres Botta <marianoabotta@gmail.com>
2020-05-13 10:53:25 -0300

    test(wrapper): agrega test visuales

---
+++ cypress/integration/wrapper.js
@@ -0,0 +1,23 @@
+/// <reference types="Cypress" />
+
+context('wrapper', () => {
+    before(() => {
+        cy.eyesOpen({ appName: 'PLEX', testName: 'plex-wrapper' });
+        cy.visit('/wrapper');
+    });
+
+    it('test accordion', () => {
+        cy.eyesCheckWindow('plex-wrapper - start');
+
+        cy.get('plex-layout-main plex-wrapper').first().plexButtonIcon('chevron-down').click();
+
+        cy.get('plex-layout-main plex-wrapper').eq(1).find('plex-button i.mdi.md-chevron-down').should('not.exist');
+
+        cy.get('plex-layout-sidebar plex-wrapper').first().plexButtonIcon('chevron-down').click();
+
+        cy.eyesCheckWindow('plex-wrapper - end');
+
+        cy.eyesClose();
+
+    });
+});
