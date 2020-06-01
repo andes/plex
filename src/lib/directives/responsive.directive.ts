a4d561bead225324820515fdbcc009249929f8ac
Andrés Boc-Ho <andresin@gmail.com>
2020-06-01 15:47:38 -0300

    feat(responsive): se mejoran breakpoints

--- src/lib/directives/responsive.directive.ts
+++ src/lib/directives/responsive.directive.ts
@@ -14,16 +14,19 @@ export class ResponsiveDirective implements AfterViewChecked {
 
     checkDimension() {
         this.width = this.el.nativeElement.clientWidth;
-        this.render.removeClass(this.el.nativeElement, 'size-xs');
+        this.render.removeClass(this.el.nativeElement, 'size-sm');
         this.render.removeClass(this.el.nativeElement, 'size-md');
+        this.render.removeClass(this.el.nativeElement, 'size-lg');
         this.render.removeClass(this.el.nativeElement, 'size-xl');
 
         if (this.width >= 1024) {
             this.render.addClass(this.el.nativeElement, 'size-xl');
         } else if (this.width >= 768 && this.width < 1024) {
+            this.render.addClass(this.el.nativeElement, 'size-lg');
+        } else if (this.width >= 576 && this.width < 768) {
             this.render.addClass(this.el.nativeElement, 'size-md');
-        } else if (this.width < 768) {
-            this.render.addClass(this.el.nativeElement, 'size-xs');
+        } else if (this.width < 576) {
+            this.render.addClass(this.el.nativeElement, 'size-sm');
         }
     }
 
