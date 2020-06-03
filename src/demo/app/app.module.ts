31273b1bf50b94fb949c2ca52895ba6e0e7722ec
Mariano Andres Botta <marianoabotta@gmail.com>
2020-06-03 13:11:44 -0300

    fix(plex): quita BrowserAnimationsModule

--- src/demo/app/app.module.ts
+++ src/demo/app/app.module.ts
@@ -1,7 +1,8 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { routing, appRoutingProviders } from './app.routes';
 import { AppComponent } from './app.component';
 
@@ -94,7 +95,7 @@ import { TooltipHintDemoComponent } from './tooltip-hint/tooltip-hint.component'
         HeaderPacienteComponent
     ],
     imports: [
-        BrowserModule,
+        BrowserAnimationsModule,
         FormsModule,
         HttpModule,
         PlexModule,
