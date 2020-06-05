a27625c24f199028bd747daad4e7d252c9d3937a
Andrés <andresin@gmail.com>
2020-06-05 10:00:40 -0300

    feat(plex-title): modo responsive de botonera (#167)
    * feat(plex-title): modo responsive de botonera
    
    * feat(responsive): se mejoran breakpoints
    
    * fix(plex-help): se arregla problema con la latura en plex-title

--- src/lib/title/title.component.ts
+++ src/lib/title/title.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, Renderer } from '@angular/core';
 @Component({
     selector: 'plex-title',
     template: `
-        <div class="plex-title d-flex flex-row justify-content-between align-items-center">
+        <div class="plex-title d-flex flex-row justify-content-between align-items-center" responsive>
             <div class="plex-title-label {{ size }}"> {{ titulo }} </div>
             <div class="title-content">
                 <ng-content></ng-content>
