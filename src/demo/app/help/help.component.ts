2300ba33dafdb965f180215a1e98eb556d021eed
Mariano Andres Botta <marianoabotta@gmail.com>
2020-06-09 12:18:48 -0300

    test(plex-help): directiva plHelp

--- src/demo/app/help/help.component.ts
+++ src/demo/app/help/help.component.ts
@@ -1,10 +1,21 @@
 import { Plex } from '../../../lib/core/service';
 import { Component, OnInit } from '@angular/core';
+import { from } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Component({
     templateUrl: 'help.html',
 })
 export class HelpDemoComponent implements OnInit {
+
+    showContent = false;
+
+    asyncContent = from([1, 2, 3, 4]).pipe(
+        tap((index) => {
+            // tslint:disable-next-line:no-console
+            console.log('Contenido evaluado recien al abrirse', index);
+        })
+    );
 
     constructor(private plex: Plex) { }
 
