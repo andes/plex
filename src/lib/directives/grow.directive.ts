311029357f154d204782eada7ccb632b44a1898b
Julio Nicolás Santarelli <santarellijulio@gmail.com>
2020-07-08 10:27:17 -0300

    fix(plex-wrapper): soporte plex-int (#187)

--- src/lib/directives/grow.directive.ts
+++ src/lib/directives/grow.directive.ts
@@ -7,27 +7,27 @@ import { Directive, Input, HostBinding } from '@angular/core';
 export class GrowDirective {
     @Input() grow: 'auto' | '1' | '2' | '3' | '4' | 'full' = 'auto';
 
-    @HostBinding('class.grid-columns-full') get full() {
+    @HostBinding('class.grow-full') get full() {
         return this.grow === 'full';
     }
 
-    @HostBinding('class.grid-columns-auto') get auto() {
+    @HostBinding('class.grow-auto') get auto() {
         return this.grow === 'auto';
     }
 
-    @HostBinding('class.grid-columns-span-1') get grow1() {
+    @HostBinding('class.grow-1') get grow1() {
         return this.grow === '1';
     }
 
-    @HostBinding('class.grid-columns-span-2') get grow2() {
+    @HostBinding('class.grow-2') get grow2() {
         return this.grow === '2';
     }
 
-    @HostBinding('class.grid-columns-span-3') get grow3() {
+    @HostBinding('class.grow-3') get grow3() {
         return this.grow === '3';
     }
 
-    @HostBinding('class.grid-columns-span-4') get grow4() {
+    @HostBinding('class.grow-4') get grow4() {
         return this.grow === '4';
     }
 }
