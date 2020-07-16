9aed961367fc8bfe9828d0d570f961661b7b3183
Andrés Boc-Ho <andresin@gmail.com>
2020-07-16 11:47:31 -0300

    refactor(hint): se reemplaza contenedor a span

--- src/lib/hint/hint.component.ts
+++ src/lib/hint/hint.component.ts
@@ -4,9 +4,9 @@ import { PlexType } from '../core/plex-type.type';
 @Component({
     selector: 'plex-hint',
     template: `
-        <a href="javascript:void(0)" *ngIf="position && content" class="hint-container detach-{{detach}}" [matTooltip]="content" [matTooltipPosition]="position">
+        <span *ngIf="position && content" class="hint-container detach-{{detach}}" [matTooltip]="content" [matTooltipPosition]="position">
             <plex-icon class="hint {{ hintType }}" [name]="icon" type="default"></plex-icon>
-        </a>
+        </span>
     `
 })
 export class HintComponent implements OnInit {
@@ -39,5 +39,4 @@ export class HintComponent implements OnInit {
     @HostListener('click', ['$event']) onClick() {
         this.hostElement.click();
     }
-
 }
