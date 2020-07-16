79c5ded3b56a24e736a252fa8cf5595277471a4e
Andrés Boc-Ho <andresin@gmail.com>
2020-07-16 11:48:41 -0300

    feat(demo hint): ejemplo de (click)

--- src/lib/icon/icon.component.ts
+++ src/lib/icon/icon.component.ts
@@ -7,8 +7,9 @@ import { Component, Input } from '@angular/core';
 export class PlexIconComponent {
     @Input() prefix = 'mdi';
     @Input() name: string;
-    @Input() type: 'success' | 'info' | 'warning' | 'danger' | 'default' = 'default';
-    @Input() size: '18' | '24' | '36' | '48';
+    @Input() type: 'light' | 'dark' | 'success' | 'info' | 'warning' | 'danger' | 'default' = 'light';
+    // Usar números con mdi, valores string con otro prefix
+    @Input() size: '18' | '24' | '36' | '48' | 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl' = 'sm';
 
     constructor() {
 
