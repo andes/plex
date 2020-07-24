f029eeaaf08f34668164466e21b1c8baf2d0a160
Andrés Boc-Ho <andresin@gmail.com>
2020-07-24 14:38:35 -0300

    feat(plex-help): mejora posicionamiento y título
    Posicionamiento para usos como en nav-item y título con plex-title

--- src/lib/help/help.component.ts
+++ src/lib/help/help.component.ts
@@ -1,23 +1,20 @@
-import { Component, Input, Renderer2, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Renderer2, Output, EventEmitter, ElementRef } from '@angular/core';
 
 @Component({
     selector: 'plex-help',
     template: `
-    <div class="toggle-{{ type }}" [ngClass]="{'closed': closed, 'open': !closed}">
-        <plex-button *ngIf="!closed" type="danger" [size]="size" icon="close" (click)="toogle();$event.stopImmediatePropagation();"></plex-button>
-        <plex-button *ngIf="content && closed && !tituloBoton" type="info" [size]="size" [title]="title" [icon]="type === 'info'? 'informacion' : 'help'" (click)="toogle();$event.stopImmediatePropagation();"></plex-button>
-        <plex-button *ngIf="content && closed && tituloBoton" type="info" [size]="size" [label]="tituloBoton" (click)="toogle();$event.stopImmediatePropagation();"></plex-button>
-    </div>
-    <div class="card help" [ngClass]="{'open': !closed, 'full': cardSize === 'full', 'half': cardSize === 'half'}" (click)="$event.stopImmediatePropagation();">
-        <ng-container *ngIf="!closed">
-            <div class="card-header">
-                <h5>{{ titulo }}</h5>
-            </div>
-            <div class="card-body m-3">
-                <h6 *ngIf="type === 'info' && subtitulo">{{ subtitulo }}</h6>
-                <ng-content></ng-content>
-            </div>
-        </ng-container>
+    <div class="toggle-help" [ngClass]="{'closed': closed, 'open': !closed}">
+        <plex-button *ngIf="!closed" type="danger" [size]="size" icon="close" (click)="toggle();$event.stopImmediatePropagation();"></plex-button>
+        <plex-button *ngIf="content && closed && !tituloBoton" type="info" [size]="size" title="{{ title }}" [icon]="type === 'info'? 'information-variant' : 'help'" (click)="toggle();$event.stopImmediatePropagation();"></plex-button>
+        <div class="card help" [ngClass]="{'open': !closed, 'full': cardSize === 'full', 'half': cardSize === 'half'}" (click)="$event.stopImmediatePropagation();">
+            <ng-container *ngIf="!closed">
+                <div class="card-body m-3">
+                    <plex-title *ngIf="titulo" size="sm" [titulo]="titulo"></plex-title>
+                    <h6 *ngIf="type === 'info' && subtitulo">{{ subtitulo }}</h6>
+                    <ng-content></ng-content>
+                </div>
+            </ng-container>
+        </div>
     </div>
     `
 })
@@ -48,21 +45,43 @@ export class PlexHelpComponent {
 
     closed = true;
 
-    constructor(private renderer: Renderer2) { }
+    constructor(private renderer: Renderer2, private el: ElementRef) { }
 
     get content() {
         return (this.icon && this.icon.length > 0) || (this.tituloBoton && this.tituloBoton.length > 0);
     }
 
-    public toogle() {
+    public toggle() {
+
+        const card = this.el.nativeElement.querySelector('.card');
+        const toggle = this.el.nativeElement.querySelector('.toggle-help');
+        const btn = toggle.querySelector('plex-button');
+
+        const toggleRect = toggle.getBoundingClientRect();
+        const cardRect = card.getBoundingClientRect();
+
         this.closed = !this.closed;
         if (!this.closed) {
+            const right = cardRect.right - cardRect.left - cardRect.width + toggleRect.width;
+            let top;
+            if (btn) {
+                const btnRect = btn.getBoundingClientRect();
+                top = toggleRect.top - (btnRect.height * 2);
+            } else {
+                top = 21;
+            }
+            this.renderer.setStyle(toggle, 'top', `${top}px`);
+            this.renderer.setStyle(toggle, 'right', `${right}px`);
+            this.renderer.setStyle(card, 'display', 'flex');
             this.open.emit();
             this.unlisten = this.renderer.listen('document', 'click', (event) => {
-                this.toogle();
+                this.toggle();
                 this.unlisten();
             });
         } else {
+            this.renderer.removeStyle(toggle, 'right');
+            this.renderer.removeStyle(toggle, 'top');
+
             this.close.emit();
             if (this.unlisten) {
                 this.unlisten();
