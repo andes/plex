4b1b0188b3a11ee09f7f45e25903d5741e86a815
Andrés Boc-Ho <andresin@gmail.com>
2020-08-07 10:42:59 -0300

    feat(plex-help): se extienden tests

--- cypress/integration/help.js
+++ cypress/integration/help.js
@@ -6,15 +6,35 @@ context('help', () => {
         cy.visit('/help');
     });
 
-    it('abre y cierra help', () => {
+    it('Abre y cierra help', () => {
 
-        cy.get('plex-layout plex-help:first').plexButtonIcon('help-circle').click();
+        const helpOpen = cy.get('plex-layout plex-help:first').plexButtonIcon('information-variant');
+        helpOpen.click();
+        cy.get('.card-body:first').should('contain.text', 'Organización');
 
-        cy.get('plex-layout plex-help').plexButtonIcon('informacion').click();
+        const helpClose = cy.get('plex-layout plex-help:first').plexButtonIcon('close');
+        helpClose.click();
 
         cy.eyesCheckWindow('plex-help');
 
         cy.eyesClose();
 
+    });
+
+    it('Falla porque el botón está oculto por otro panel de plex-help', (done) => {
+
+        const helpOpen = cy.get('plex-layout plex-help:first').plexButtonIcon('information-variant');
+        helpOpen.click();
+
+        const helpOpenFail = cy.get('plex-layout plex-help').eq(1).plexButtonIcon('help');
+        helpOpenFail.click();
+
+        cy.once('fail', (err) => {
+
+            expect(err.message).to.include('`cy.click()` failed because this element');
+            expect(err.message).to.include('is being covered by another element');
+
+            done();
+        });
     });
 });
