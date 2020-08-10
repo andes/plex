81be27a37d52968c378287e7734746a0de9813e2
Andrés Boc-Ho <andresin@gmail.com>
2020-08-10 12:00:21 -0300

    chore: uso de types PlexSize y PlexType

--- src/lib/icon/icon.component.ts
+++ src/lib/icon/icon.component.ts
@@ -1,3 +1,5 @@
+import { PlexSize } from './../core/plex-size.type';
+import { PlexType } from './../core/plex-type.type';
 import { Component, Input } from '@angular/core';
 
 @Component({
@@ -7,13 +9,11 @@ import { Component, Input } from '@angular/core';
 export class PlexIconComponent {
     @Input() prefix = 'adi';
     @Input() name: string;
-    @Input() type: 'light' | 'dark' | 'success' | 'info' | 'warning' | 'danger' | 'default' = 'dark';
+    @Input() type: PlexType = 'default';
     // Usar números con mdi, valores string con otro prefix
-    @Input() size: '18' | '24' | '36' | '48' | 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl' = 'sm';
+    @Input() size: '18' | '24' | '36' | '48' | PlexSize = 'sm';
 
-    constructor() {
-
-    }
+    constructor() { }
 
     get cssSize(): string {
         return `${this.prefix === 'mdi' ? `${this.prefix}-${this.size}px` : this.size}`;
