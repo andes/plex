753bd075884c035bde8149603a40d12907972f93
liquid36 <liquid36@users.noreply.github.com>
2020-09-11 19:20:37 +0000

    deploy: feab617337ae0034b7d5a1fc1d98d8be161b2bfc

--- demo/app/templates/service/paciente.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Paciente = /** @class */ (function () {
-    function Paciente() {
-    }
-    return Paciente;
-}());
-exports.Paciente = Paciente;
-//# sourceMappingURL=../../../../../src/demo/app/templates/service/paciente.js.map
--- demo/app/templates/template-data.interface.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=../../../../src/demo/app/templates/template-data.interface.js.map
