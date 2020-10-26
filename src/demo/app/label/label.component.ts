b8cc36b9ccaa9a8790d39ff9322a4d064cc3a52f
Julio Santarelli <santarellijulio@gmail.com>
2020-10-26 07:34:14 -0300

    feat(plex-label): se crea tipología timeline

--- src/demo/app/label/label.component.ts
+++ src/demo/app/label/label.component.ts
@@ -19,10 +19,17 @@ export class LabelDemoComponent {
         { label: 'edad', valor: '41 años', type: 'success' },
         { label: 'documento', valor: '29.879.253', type: 'info' },
         { label: 'sexo', valor: 'Masculino', type: 'default' },
-        { label: 'género', valor: 'Hospital Provincial de Neuquen Castro Rendon', type: 'warning' },
+        { label: 'género', valor: 'Femenino', type: 'warning' },
         { label: 'fecha de nacimiento', valor: '14 de Julio de 1953', type: 'success' },
         { label: 'CUIL', valor: '20-16879253-5', type: 'warning' },
-        { label: 'Nota', valor: 'Donec quam felis, ultricies nec, pellentesque eu, pretium quis. Lorem ipsum sonnet.', type: 'warning' },
-        { label: 'Comience buscando un paciente en la barra superior', valor: 'Ingrese al menos tres caracteres. Si la búsqueda no arroja el resultado esperado, presione el botón "Paciente Nuevo"', type: 'warning' }
+        { label: 'Nota', valor: 'Donec quam felis, ultricies nec.', type: 'warning' },
+        { label: 'Búsque y seleccione un paciente', valor: 'Ingrese al menos tres caracteres. Si la búsqueda no arroja el resultado esperado, presione el botón "Paciente Nuevo"', type: 'A' },
+        { label: 'Seleccione el profesional', valor: 'La selección se realiza desde los campos desplegables que se encuentran sobre el calendario de agendas', type: 'B' },
+        { label: 'Indique agenda y horario', valor: 'Una vez seleccionado el profesional y la prestación, podrá visualizar las opciones de bloques y horarios', type: 'C' },
+        { label: 'Aceptada por Rubén Palma', valor: 'Texto descriptivo del evento de la línea de tiempo. En el que se desarrolla el item.', type: '03/11/2020' },
+        { label: 'Validada por Carolina Celeste', valor: 'Texto descriptivo del evento de la línea de tiempo. En el que se desarrolla el item.', type: '03/07/2020' },
+        { label: 'Profesional asignado por Alberto Carranza', valor: 'Texto descriptivo del evento de la línea de tiempo. En el que se desarrolla el item.', type: '11/09/2019' },
+        { label: 'Ejecutada por Juan Ignacio Gonzalez', valor: 'Texto descriptivo del evento de la línea de tiempo. En el que se desarrolla el item.', type: '19/05/2019' },
+        { label: 'Creada por Carolina Celeste', valor: 'Texto descriptivo del evento de la línea de tiempo. En el que se desarrolla el item.', type: '01/03/2018' }
     ];
 }
--- src/lib/label/label.component.ts
+++ src/lib/label/label.component.ts
@@ -8,23 +8,25 @@ import { Component, Input } from '@angular/core';
             <ng-content select="plex-icon"></ng-content>
             <p dato *ngIf="dato && !icon"></p>
             <ng-content select="[dato]"></ng-content>
-            <section justify="center" class="d-flex w-100 text-{{ type }}" *ngIf="timeline">
-                <svg height="100%" width="1" class="timeline" [ngClass]="direction === 'column' ? 'none' : 'vertical'">
-                    <line x1="0" y1="0%" x2="0" y2="30%" style="stroke:black;stroke-width:5" />
-                </svg>
-                <svg height="100%" width="1" class="timeline" [ngClass]="direction === 'column' ? 'none' : 'vertical'">
-                    <line x1="-1" y1="70%" x2="-1" y2="100%" style="stroke:black;stroke-width:5" />
+            <section class="d-flex text-{{ type }}" *ngIf="timeline">
+                <svg height="1" width="100%" class="timeline" [ngClass]="direction === 'row' ? 'none' : ''">
+                    <line x1="0%" y1="0" x2="40%" y2="0" style="stroke:black;stroke-width:5" />
                 </svg>
                 <svg height="1" width="100%" class="timeline" [ngClass]="direction === 'row' ? 'none' : ''">
-                    <line x1="0%" y1="0" x2="30%" y2="0" style="stroke:black;stroke-width:5" />
-                </svg>
-                <svg height="1" width="100%" class="timeline" [ngClass]="direction === 'row' ? 'none' : ''">
-                    <line x1="70%" y1="-1" x2="100%" y2="-1" style="stroke:black;stroke-width:5" />
+                    <line x1="60%" y1="-1" x2="100%" y2="-1" style="stroke:black;stroke-width:5" />
                 </svg>
             </section>
-            <div class="d-flex flex-column" [ngClass]="{ 'align-items-center mt-2': direction === 'column' }">
+            <section class="ml-4" *ngIf="timeline">
+                <svg height="100%" width="1" class="timeline" [ngClass]="direction === 'column' ? 'none' : 'vertical'">
+                    <line x1="0" y1="0%" x2="0" y2="30%" style="stroke:white;stroke-width:5" />
+                </svg>
+                <svg height="100%" width="1" class="timeline" [ngClass]="direction === 'column' ? 'none' : 'vertical'">
+                    <line x1="-1" y1="70%" x2="-1" y2="100%" style="stroke:white;stroke-width:5" />
+                </svg>
+            </section>
+            <div class="d-flex flex-column" [ngClass]="direction === 'column' ? 'align-items-center mt-2 px-4' : 'my-4'">
                 <span *ngIf="titulo" class="text-{{ type }}" [ngClass]="{'font-weight-bold': tituloBold}">{{ titulo }}</span>
-                <small *ngIf="titulo && subtitulo">{{ subtitulo }}</small>
+                <small *ngIf="titulo && subtitulo" class="">{{ subtitulo }}</small>
             </div>
             <ng-content *ngIf="!titulo && !subtitulo"></ng-content>
         </div>
