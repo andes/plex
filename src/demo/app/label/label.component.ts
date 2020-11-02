3cbc7c6c9019b000d883561f2ba8821a5f968369
Julio Santarelli <santarellijulio@gmail.com>
2020-11-02 12:36:25 -0300

    feat(plex-label): permite caracteres en lugar de icono

--- src/demo/app/label/label.component.ts
+++ src/demo/app/label/label.component.ts
@@ -9,7 +9,6 @@ export class LabelDemoComponent {
     foto = true;
     icon = false;
 
-
     icono = {
         caracter: 'pencil',
         color: 'info',
@@ -23,6 +22,21 @@ export class LabelDemoComponent {
         { label: 'fecha de nacimiento', valor: '14 de Julio de 1953', type: 'success' },
         { label: 'CUIL', valor: '20-16879253-5', type: 'warning' },
         { label: 'Nota', valor: 'Donec quam felis, ultricies nec, pellentesque eu, pretium quis. Lorem ipsum sonnet.', type: 'warning' },
-        { label: 'Comience buscando un paciente en la barra superior', valor: 'Ingrese al menos tres caracteres. Si la búsqueda no arroja el resultado esperado, presione el botón "Paciente Nuevo"', type: 'warning' }
+        { label: 'Comience buscando un paciente en la barra superior', valor: 'Ingrese al menos tres caracteres. Si la búsqueda no arroja el resultado esperado, presione el botón "Paciente Nuevo"', type: 'warning' },
+    ];
+
+    pasos = [
+        { label: 'A', valor: 'Ingrese al menos tres caracteres. Si la búsqueda no arroja el resultado esperado, presione el botón "Paciente Nuevo".', type: 'Busque un paciente' },
+        { label: 'B', valor: 'La selección se realiza desde los campos desplegables que se encuentran sobre el calendario de agendas.', type: 'Seleccione prestación' },
+        { label: 'C', valor: 'Una vez seleccionado el profesional y la prestación, podrá visualizar las opciones de bloques y horarios.', type: 'Confirme horario' },
+    ];
+
+    eventos = [
+        { label: '27/11/2020', valor: 'Texto descriptivo de un item perteneciente a una línea de tiempo en el que se desarrollan varios eventos.', type: 'Asignada por Alberto Carranza' },
+        { label: '12/10/2020', valor: 'Texto descriptivo de un item perteneciente a una línea de tiempo en el que se desarrollan varios eventos.', type: 'Asignada por Alberto Carranza' },
+        { label: '09/07/2020', valor: 'Texto descriptivo de un item perteneciente a una línea de tiempo en el que se desarrollan varios eventos.', type: 'Asignada por Alberto Carranza' },
+        { label: '21/05/2020', valor: 'Texto descriptivo de un item perteneciente a una línea de tiempo en el que se desarrollan varios eventos.', type: 'Asignada por Alberto Carranza' },
+        { label: '21/12/2019', valor: 'Texto descriptivo de un item perteneciente a una línea de tiempo en el que se desarrollan varios eventos.', type: 'Asignada por Alberto Carranza' },
+        { label: '17/06/2019', valor: 'Texto descriptivo de un item perteneciente a una línea de tiempo en el que se desarrollan varios eventos.', type: 'Asignada por Alberto Carranza' },
     ];
 }
--- src/lib/label/label.component.ts
+++ src/lib/label/label.component.ts
@@ -1,29 +1,35 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
     selector: 'plex-label',
     template: `
-        <div class="plex-label {{ size }} d-flex flex-{{direction}}" [ngClass]="{ 'align-items-center': direction === 'column' }">
-            <plex-icon *ngIf="icon" name="{{icon}}" class="text-{{ type }}"></plex-icon>
-            <ng-content select="plex-icon"></ng-content>
-
-            <div class="d-flex flex-column" [ngClass]="{ 'align-items-center mt-2': direction === 'column' }">
-                <span *ngIf="titulo" class="text-{{ type }}" [ngClass]="{'font-weight-bold': tituloBold}">{{ titulo }}</span>
-                <small *ngIf="titulo && subtitulo">{{ subtitulo }}</small>
-            </div>
-            <ng-content *ngIf="!titulo && !subtitulo"></ng-content>
-        </div>
+    <div class="plex-label {{ size }} d-flex flex-{{direction}}" [ngClass]="{ 'align-items-center': direction === 'column' }">
+    <plex-icon *ngIf="icon && !datos" name="{{icon}}" class="text-{{ type }}"></plex-icon>
+    <ng-content select="plex-icon"></ng-content>
+    <ng-content select="[dato]"></ng-content>
+    <div class="d-flex flex-column" [ngClass]="direction === 'column' ? 'align-items-center mt-2 px-4' : ''">
+        <span *ngIf="titulo" class="text-{{ type }}" [ngClass]="{'font-weight-bold': tituloBold}">{{ titulo }}</span>
+        <small *ngIf="titulo && subtitulo" class="">{{ subtitulo }}</small>
+    </div>
+    <ng-content *ngIf="!titulo && !subtitulo"></ng-content>
+</div>
     `
 })
-export class PlexLabelComponent {
+export class PlexLabelComponent implements AfterViewInit {
     @Input() titulo: string;
     @Input() tituloBold = true;
     @Input() subtitulo: string;
     @Input() size: 'sm' | 'md' | 'lg' | 'xl' = 'md';
     @Input() icon: string;
+    @Input() dato: string;
     @Input() direction: 'column' | 'row' = 'row';
     @Input() type: 'success' | 'info' | 'warning' | 'danger' | 'default';
 
-    constructor() {
+    public datos = false;
+
+    ngAfterViewInit() {
+        this.datos = !!this.datRef.nativeElement.querySelector('[dato]');
     }
+
+    constructor(private datRef: ElementRef) { }
 }
