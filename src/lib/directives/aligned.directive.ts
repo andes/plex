9a788f56238598aefeffffb0fa3b6d67270f46a1
Julio Nicolás Santarelli <santarellijulio@gmail.com>
2020-11-16 18:34:08 -0300

    feat(aligned): crea directiva (#236)

---
+++ src/lib/directives/aligned.directive.ts
@@ -0,0 +1,23 @@
+import { Directive, Input, HostBinding } from '@angular/core';
+
+@Directive({
+    // tslint:disable-next-line:directive-selector
+    selector: '[aligned]'
+})
+export class AlignedDirective {
+    @Input() aligned: 'start' | 'end' | 'center' = 'center';
+
+    @HostBinding('class.d-flex') flex = true;
+
+    @HostBinding('class.align-items-start') get start() {
+        return this.aligned === 'start';
+    }
+
+    @HostBinding('class.align-items-end') get end() {
+        return this.aligned === 'end';
+    }
+
+    @HostBinding('class.align-items-center') get center() {
+        return !this.aligned || this.aligned === 'center';
+    }
+}
