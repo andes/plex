3b42f31265050795a2b19ded774f390b7faaf598
Julio Santarelli <santarellijulio@gmail.com>
2020-12-14 12:17:00 -0300

    feat(button-badge): redondea bordes, pruebas iniciales

--- src/lib/badge/badge.component.ts
+++ src/lib/badge/badge.component.ts
@@ -1,15 +1,16 @@
 import { PlexSize } from './../core/plex-size.type';
 import { PlexType } from './../core/plex-type.type';
-import { Component, Input, ElementRef, ViewChild, OnChanges } from '@angular/core';
+import { Component, Input, ElementRef, ViewChild, OnChanges, ContentChildren, QueryList } from '@angular/core';
+import { PlexButtonComponent } from '../button/button.component';
 
 @Component({
     selector: 'plex-badge',
     template: `
-        <span #badgeIcon class="badge badge-{{ type }} badge-{{ size }}">
+        <span #badgeIcon class="badge badge-{{ type }} badge-{{ size }}" [class.badge-btn]="hasButton()">
             <ng-content select="plex-icon"></ng-content>
             <ng-content></ng-content>
         </span>
-        <span #badgeBtn class="btn-badge btn-badge-{{ type }}">
+        <span *ngIf="hasButton()" #badgeBtn class="btn-badge btn-badge-{{ type }}">
             <ng-content select="plex-button"></ng-content>
             <ng-content select="plex-datetime"></ng-content>
         </span>
@@ -19,6 +20,7 @@ export class PlexBadgeComponent implements OnChanges {
     @Input() type: PlexType;
     @Input() size: PlexSize = 'md';
     @Input() color: string;
+    @ContentChildren(PlexButtonComponent, { descendants: false }) plexButtons: QueryList<PlexButtonComponent>;
 
     @ViewChild('badgeIcon', { static: true }) badgeIcon: ElementRef;
 
@@ -33,7 +35,9 @@ export class PlexBadgeComponent implements OnChanges {
             this.badgeIcon.nativeElement.style.setProperty('--badge-color', this.color);
             this.badgeBtn.nativeElement.style.setProperty('--badge-color', this.color);
         }
-
     }
 
+    hasButton() {
+        return this.plexButtons.length > 0;
+    }
 }
