6c5adad698c3242daada9707c243e25d1a92b674
Mariano Andres Botta <marianoabotta@gmail.com>
2021-03-22 11:11:35 -0300

    fix(plex-visualizador): exporta servicio

--- index.ts
+++ index.ts
@@ -4,3 +4,4 @@ export * from './src/lib/dropdown/dropdown-item.inteface';
 export * from './src/lib/select/select-event.interface';
 export * from './src/lib/options/options.component';
 export * from './src/lib/text/text.component';
+export * from './src/lib/core/plex-visualizador.service';
