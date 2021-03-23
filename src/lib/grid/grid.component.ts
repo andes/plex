c28d4dc06509875c9a13b7d6262147696d6ac7f7
Julio Santarelli <santarellijulio@gmail.com>
2021-03-23 08:20:26 -0300

    feat(plex-grid): agrega size auto y breakpoints

--- src/lib/grid/grid.component.ts
+++ src/lib/grid/grid.component.ts
@@ -4,21 +4,24 @@ import { PlexLabelComponent } from '../label/label.component';
 @Component({
     selector: 'plex-grid',
     template: `
-            <ng-content></ng-content>
+            <section class="cols-{{ cols }} grid-column-span-{{ span }} size-{{ size }} type-{{ type }}" responsive>
+               <ng-content></ng-content>
+            </section>
     `,
 })
 
 export class PlexGridComponent implements AfterViewChecked {
 
-    /**
-     * Propiedades desde CSS
-     *
-     * type= auto|fill
-     * size= xs|sm|md|lg|xl
-     * cols= 2|3|4|5|6
-     * span= 2|3|4|5|6
-     *
-     */
+    @Input() type: 'auto' | 'full';
+    @Input() size: 'auto' | 'xs' | 'sm' | 'md' | 'lg' | 'xl' = 'auto';
+
+    @Input() cols: '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | '11' | '12';
+    @Input() span: '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | '11' | '12';
+
+
+    @Input() colSm: '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | '11' | '12';
+    @Input() colMd: '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | '11' | '12';
+    @Input() colLg: '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | '11' | '12';
 
     @ContentChildren(PlexLabelComponent) plexLabels: QueryList<PlexLabelComponent>;
     @ContentChildren(PlexLabelComponent, { read: ElementRef }) plexLabelsElement: QueryList<ElementRef>;
