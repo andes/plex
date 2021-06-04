3efb4ee61889a95a98e50c58ed50653991b02bc1
Andrés Boc-Ho <andresin@gmail.com>
2021-06-04 11:38:01 -0300

    refactor(hint): por cambios en HTML del comp

--- cypress/integration/directiva-hint.js
+++ cypress/integration/directiva-hint.js
@@ -11,12 +11,12 @@ context('directive hint', () => {
     it('hint directive content', () => {
         const hintText = '¿Sabías que... el hint es un texto que sugiere acciones o indica alguna novedad?';
         cy.get('#cdk-describedby-message-container div').eq(0).should('not.be.visible');
-        cy.get('plex-hint').eq(0).find('a').focus().trigger('mouseover', { force: true });
+        cy.get('plex-hint').eq(0).find('span').focus().trigger('mouseover', { force: true });
         cy.get('#cdk-describedby-message-container div').eq(0).should('contain', hintText);
     });
 
     it.only('hint directive click', () => {
-        cy.get('plex-hint').eq(0).find('a').focus().trigger('click', { force: true });
+        cy.get('plex-hint').eq(0).find('span').focus().trigger('click', { force: true });
         cy.get('div.hint-click-test').should('contain', '¿Cuántos clicks fallidos hace uno en la vida?');
     });
 
