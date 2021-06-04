eda614cf38fad6bdaafff98701b45c68d56f7782
Andrés Boc-Ho <andresin@gmail.com>
2021-06-04 11:21:15 -0300

    feat(hint): se muestra tooltip de hint sin delay

--- src/lib/hint/hint.component.ts
+++ src/lib/hint/hint.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit, Input, HostListener } from '@angular/core';
+import { MatTooltip } from '@angular/material/tooltip';
+import { Component, OnInit, Input, HostListener, ViewChild } from '@angular/core';
 import { PlexType } from '../core/plex-type.type';
 
 @Component({
     selector: 'plex-hint',
     template: `
-        <span tabindex="0" *ngIf="position && content" class="hint-container detach-{{detach}}" [matTooltip]="content" [matTooltipPosition]="position" [matTooltipShowDelay]="0">
+        <span #matTooltip="matTooltip" tabindex="0" *ngIf="position && content" class="hint-container detach-{{detach}}" 
+        [matTooltip]="content" [matTooltipPosition]="position" (click)="showTooltip()">
             <plex-icon class="hint {{ hintType }}" [name]="hintIcon" size="xs" type="light"></plex-icon>
         </span>
     `
@@ -31,8 +33,14 @@ export class HintComponent implements OnInit {
 
     constructor() { }
 
+    @ViewChild('matTooltip', { static: false }) matTooltip: MatTooltip;
+
     ngOnInit() {
         this.position = 'above';
+    }
+
+    showTooltip() {
+        this.matTooltip.show(0);
     }
 
     // Si el elemento que tiene la directiva [hint] tiene un evento (click), este se ejecutará, guste o no.
