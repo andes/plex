735463d9aba39009ade6c3f20a0751d6c8020511
Julio Nicolás Santarelli <santarellijulio@gmail.com>
2021-08-20 14:49:50 -0300

    feat(plex-accordion): posibilita toggle alternado (#293)
    * feat(plex-accordion): posibilita toggle alternado
    
    * fix(datetime): espaciado top de días
    
    Co-authored-by: Andes <andesappsalud@gmail.com>

--- src/lib/accordion/accordion.component.ts
+++ src/lib/accordion/accordion.component.ts
@@ -1,5 +1,6 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { AfterContentInit, Component, Input, OnDestroy } from '@angular/core';
 import { PlexPanelComponent } from './panel.component';
+
 
 @Component({
     selector: 'plex-accordion',
@@ -8,10 +9,34 @@ import { PlexPanelComponent } from './panel.component';
                 </div>
                 `,
 })
-export class PlexAccordionComponent {
+
+export class PlexAccordionComponent implements AfterContentInit, OnDestroy {
+    @Input() activeLast: boolean;
     public panels: PlexPanelComponent[] = [];
 
     addPanel(panel: PlexPanelComponent) {
         this.panels.push(panel);
+    }
+
+    ngAfterContentInit() {
+        this.panels.forEach((panel) => {
+            panel.toggle.subscribe((open) => {
+                if (this.activeLast && open) {
+                    this.openPanel(panel);
+                }
+            });
+        });
+    }
+
+    openPanel(panel: PlexPanelComponent) {
+        this.panels.filter(p => p !== panel).forEach(p => p.active = false);
+    }
+
+    ngOnDestroy() {
+        if (this.panels) {
+            this.panels.forEach((panel) => {
+                panel.toggle.unsubscribe();
+            });
+        }
     }
 }
