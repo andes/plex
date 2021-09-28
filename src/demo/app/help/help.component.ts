d9c167a033b144ddd8916eba6e99112fb3211ded
Julio Santarelli <santarellijulio@gmail.com>
2021-09-28 16:11:05 -0300

    docs(plex-help): agrega ejemplo con toggle

--- src/demo/app/help/help.component.ts
+++ src/demo/app/help/help.component.ts
@@ -1,14 +1,24 @@
-import { Plex } from '../../../lib/core/service';
 import { Component, OnInit } from '@angular/core';
-import { from } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { from, Observable } from 'rxjs';
+import { tap, switchMap } from 'rxjs/operators';
+import { PlexHelpComponent } from '../../../lib/help/help.component';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+
+// Mock data
+import { PacienteService } from './../../app/templates/service/paciente.service';
+import { Paciente } from '../templates/service/paciente';
 
 @Component({
     templateUrl: 'help.html',
 })
 export class HelpDemoComponent implements OnInit {
 
+    showItem = false;
     showContent = false;
+
+    pacientes$: Observable<Paciente[]>;
+    paciente$: Observable<Paciente>;
+    selectedId: string;
 
     asyncContent = from([1, 2, 3, 4]).pipe(
         tap((index) => {
@@ -17,8 +27,26 @@ export class HelpDemoComponent implements OnInit {
         })
     );
 
-    constructor(private plex: Plex) { }
+    constructor(
+        private pacienteService: PacienteService,
+        private route: ActivatedRoute,
+        private router: Router
+    ) { }
 
     ngOnInit() {
+        this.pacientes$ = this.pacienteService.getPacientes();
+
+        this.paciente$ = this.route.paramMap.pipe(
+            switchMap((params: ParamMap) =>
+                this.pacienteService.getPaciente(params.get('id')))
+        );
     }
+
+    agregarItem(paciente, helpTest: PlexHelpComponent) {
+        helpTest.toggle();
+        this.showItem = !this.showItem;
+        this.selectedId = paciente.id;
+        this.router.navigate(['help', this.selectedId]);
+    }
+
 }
