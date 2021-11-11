9bf5227d25c51e1e9122010500460b2de3f97479
Andrés Boc-Ho <andresin@gmail.com>
2021-11-11 09:38:20 -0300

    feat(plex-help): modalidad auto sin overflow

--- src/lib/help/help.component.ts
+++ src/lib/help/help.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, Renderer2, Output, EventEmitter } from '@angular/core';
+import { PlexType } from './../core/plex-type.type';
+import { Component, Input, Renderer2, Output, EventEmitter, ElementRef } from '@angular/core';
 
 @Component({
     selector: 'plex-help',
@@ -8,8 +9,8 @@ import { Component, Input, Renderer2, Output, EventEmitter } from '@angular/core
     </plex-button>
     <plex-button class="btn-open" *ngIf="content && closed && tituloBoton" [type]="btnType" [size]="size" [label]="tituloBoton" (click)="toggle();$event.stopImmediatePropagation();">
     </plex-button>
-    <div class="toggle-help" [ngClass]="{'closed': closed, 'open': !closed}">
-        <div class="card help" [ngClass]="{'open': !closed, 'full': cardSize === 'full', 'half': cardSize === 'half'}" (click)="$event.stopImmediatePropagation();">
+    <div class="toggle-help" [ngClass]="{'closed': closed, 'open': !closed}" #plHelpBody>
+        <div class="card help" [class.open]="!closed" [class.closed]="closed" [ngClass]="cardSize" (click)="$event.stopImmediatePropagation();">
             <ng-container *ngIf="!closed">
                 <div class="card-body m-3">
                     <plex-title *ngIf="titulo" size="sm" [titulo]="titulo"></plex-title>
@@ -30,7 +31,7 @@ export class PlexHelpComponent {
 
     @Input() size: 'sm' | 'md' = 'sm';
 
-    @Input() cardSize: 'full' | 'half' = 'full';
+    @Input() cardSize: 'full' | 'half' | 'auto' = 'full';
 
     @Input() title: string;
 
@@ -50,16 +51,27 @@ export class PlexHelpComponent {
 
     closed = true;
 
-    constructor(private renderer: Renderer2) { }
+    constructor(
+        private elementRef: ElementRef,
+        private renderer: Renderer2
+    ) { }
 
     get content() {
         return (this.icon && this.icon.length > 0) || (this.tituloBoton && this.tituloBoton.length > 0);
     }
 
     public toggle() {
-
         this.closed = !this.closed;
         if (!this.closed) {
+
+            setTimeout(() => {
+                const toggleDiv = this.elementRef.nativeElement.querySelector('div.toggle-help');
+                const auto = toggleDiv.querySelector('.auto');
+                if (auto) {
+                    const offset = toggleDiv.querySelector('.card').getBoundingClientRect().top + 40;
+                    toggleDiv.querySelector('div.card-body').style.height = `calc(100vh - ${offset}px)`;
+                }
+            }, 0);
 
             this.open.emit();
             this.unlisten = this.renderer.listen('document', 'click', (event) => {
