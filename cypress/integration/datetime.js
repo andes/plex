e5af7508d7f4003c65b61c139ff7922c2479fd07
Andrés <andresin@gmail.com>
2021-11-17 09:49:46 -0300

    fix(plex-datetime): ajuste visual en hint (#301)
    * fix(hint-datetime): se corrigen problemas visuales
    
    * fix(datetime): corrige de detección de cambios
    
    * refactor(datetime): se aplica plex-layout
    
    * refactor(datetime): funcionan con plex-layout
    
    * feat(plex-help): modalidad auto sin overflow
    
    * feat(plex-help): implementa types de botones
    
    * Revert "feat(plex-help): modalidad auto sin overflow"
    
    This reverts commit fe45753dba3213ef87899e6e87c697fe236c72a3.
    
    * Revert "feat(plex-help): implementa types de botones"
    
    This reverts commit c202e75bad05efe528937451246e04a8e450d13e.
    
    * feat(plex-datetime-hint): permite custom text
    
    Co-authored-by: Andes <andesappsalud@gmail.com>

--- cypress/integration/datetime.js
+++ cypress/integration/datetime.js
@@ -14,12 +14,12 @@ context('datetime', () => {
         cy.plexDatetime('label="Fecha menor a"').click();
         cy.plexDatetime('label="Ingrese la fecha y la hora del evento"').find('input').should('have.value', '01/01/2019 00:00');
 
-        cy.get('.col-md-6').contains('2019-01-01T03:00:00.000Z');
+        cy.get('plex-layout-sidebar pre').contains('2019-01-01T03:00:00.000Z');
 
         cy.plexDatetime('label="Fecha menor a"', '01/01/1950');
         cy.plexDatetime('label="Fecha menor a"').validationMessage('El valor debe ser mayor a');
 
-        cy.get('.col-md-6').contains('1950-01-01T03:00:00.000Z');
+        cy.get('plex-layout-sidebar pre').contains('1950-01-01T03:00:00.000Z');
 
         cy.plexDatetime('name="hora"', '13:00');
         cy.plexDatetime('name="horaconmin"', '10:00');
@@ -44,7 +44,7 @@ context('datetime', () => {
         cy.get('.dtp-picker-datetime').find('[id="m-0"]').click({ multiple: true, force: true });
 
         cy.plexDatetime('name="fechaSkip"').find('input').should('have.value', '31/01/1970 00:00');
-        cy.get('.col-md-6').contains('1970-01-31T03:00:00.000Z');
+        cy.get('plex-layout-sidebar pre').contains('1970-01-31T03:00:00.000Z');
 
         cy.eyesCheckWindow({ tag: 'datetime - end', ignore: { selector: 'pre' } });
 
