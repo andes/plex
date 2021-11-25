4e86219c5b3c1200ca365cb8093949524fcba051
Juliana <juli.otero01@gmail.com>
2021-11-25 16:40:21 -0300

    feat(plex): sumar tipos a plex-title

--- src/lib/title/title.component.ts
+++ src/lib/title/title.component.ts
@@ -1,10 +1,11 @@
+import { PlexType } from './../core/plex-type.type';
 import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'plex-title',
     template: `
         <div role="heading" [attr.aria-level]="ariaLevel" class="plex-title d-flex flex-row justify-content-between align-items-center" responsive>
-            <div class="plex-title-label {{ size }}"> {{ titulo }} </div>
+            <div class="plex-title-label {{ size }} {{cssType}}"> {{ titulo }} </div>
             <div class="title-content">
                 <ng-content></ng-content>
             </div>
@@ -18,10 +19,15 @@ import { Component, Input } from '@angular/core';
 export class PlexTitleComponent {
     @Input() titulo: string;
     @Input() size: 'sm' | 'md' | 'xl' | 'lg' = 'lg';
+    @Input() type: PlexType = 'info';
 
     // Asigna un aria-level al heading según su tamaño
     get ariaLevel() {
         return (this.size === 'xl' || this.size === 'lg') ? 1 : (this.size === 'md' ? 2 : 3);
+    }
+
+    get cssType(): string {
+        return this.type ? 'text-' + this.type : '';
     }
 
     constructor() {
