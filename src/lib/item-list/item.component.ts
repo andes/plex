74f49086663f2cf7db672d29036f9469ac0f556d
palita1991 <marianopalavecino7@gmail.com>
2022-03-09 14:02:03 -0300

    feat(plex-item): configuracion de plex item en una sola linea

--- src/lib/item-list/item.component.ts
+++ src/lib/item-list/item.component.ts
@@ -4,34 +4,32 @@ import { PlexIconComponent } from '../icon/icon.component';
 import { PlexBoolComponent } from '../bool/bool.component';
 import { PlexBadgeComponent } from '../badge/badge.component';
 import { PlexButtonComponent } from '../button/button.component';
+import { PlexLabelComponent } from '../label/label.component';
 
 @Component({
     selector: 'plex-item',
     template: `
         <section #item class="item" [class.custom-colors]="hasColors" [class.selectable]="selectable" [class.selected]="selectable && selected">
-            <div class="item-row">
-                <div class="elementos-graficos">
+            <div class="" style="display: flex !important; justify-content: space-between !important; flex-wrap:wrap-reverse;">
+                <div class="item-list"
+                    [class.has-icon]="plexIcons?.length > 0 || imgs || svgs"
+                    [class.has-checkbox]="plexBools?.length > 0">
                     <ng-content select="plex-bool"></ng-content>
                     <ng-content select="plex-icon"></ng-content>
                     <ng-content select="img"></ng-content>
                     <ng-content select="svg"></ng-content>
-                </div>
-                <div class="item-list"
-                    [class.has-icon]="plexIcons?.length > 0 || imgs || svgs"
-                    [class.has-checkbox]="plexBools?.length > 0"
-                    >
                     <ng-content></ng-content>
                 </div>
-            </div>
-            <div *ngIf="hasBotonera()" class="botonera">
-                <div>
-                    <ng-content select="plex-badge"></ng-content>
-                </div>
-                <div>
-                    <ng-content select="plex-button"></ng-content>
-                    <ng-content select="upload-file"></ng-content>
-                    <ng-content select="plex-dropdown[icon]"></ng-content>
-                </div>
+                <div *ngIf="hasBotonera()" class="botonera d-flex justify-content-between align-items-center">
+                    <div class="d-flex justify-content-start align-items-center">
+                        <ng-content select="plex-badge"></ng-content>
+                    </div>
+                    <div class="d-flex justify-content-end align-items-center">
+                        <ng-content select="plex-button"></ng-content>
+                        <ng-content select="upload-file"></ng-content>
+                        <ng-content select="plex-dropdown[icon]"></ng-content>
+                    </div>
+                </div> 
             </div>
         </section>
     `
@@ -50,6 +48,7 @@ export class PlexItemComponent implements AfterViewInit {
     @ContentChildren(PlexBoolComponent, { descendants: false }) plexBools: QueryList<PlexBoolComponent>;
     @ContentChildren(PlexBadgeComponent, { descendants: false }) plexBadges: QueryList<PlexBadgeComponent>;
     @ContentChildren(PlexButtonComponent, { descendants: false }) plexButtons: QueryList<PlexButtonComponent>;
+    @ContentChildren(PlexLabelComponent, { descendants: false }) plexLabels: QueryList<PlexLabelComponent>;
 
     @ViewChild('item', { static: true }) item: ElementRef;
 
