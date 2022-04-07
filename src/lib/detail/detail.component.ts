f87d666963f96bb28fe0a23a5a8ede5ee5640422
palita1991 <marianopalavecino7@gmail.com>
2022-04-07 14:29:04 -0300

    feat(plex-detail): agrega botonera en detalle

--- src/lib/detail/detail.component.ts
+++ src/lib/detail/detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, ViewChildren, QueryList, AfterContentInit, AfterViewChecked, ContentChildren, ElementRef, Renderer2 } from '@angular/core';
 import { PlexLabelComponent } from '../label/label.component';
+import { PlexButtonComponent } from '../button/button.component';
 
 @Component({
     selector: 'plex-detail',
@@ -10,8 +11,14 @@ import { PlexLabelComponent } from '../label/label.component';
                 <ng-content select="img"></ng-content>
             </div>
             <div class="contenedor-textos" [ngClass]="{ 'd-flex flex-column': direction === 'column'  }">
-                <span class="d-flex flex-row flex-wrap">
-                    <ng-content select="plex-badge"></ng-content>
+                <span class="d-flex flex-row flex-wrap justify-content-between">
+                    <span class="d-flex align-items-start justify-content-start flex-row flex-wrap cont-badge px-0">
+                        <ng-content select="plex-badge"></ng-content>
+                    </span>
+                    <span *ngIf="hasButton()" class="d-flex align-items-start justify-content-end flex-row flex-wrap cont-button px-0">
+                        <ng-content select="plex-button[size=sm]"></ng-content>
+                        <ng-content select="plex-dropdown"></ng-content>
+                    </span>
                 </span>
                 <ng-content select="div[title]"></ng-content>
                 <ng-content select="div[subtitle]"></ng-content>
@@ -34,9 +41,14 @@ export class PlexDetailComponent implements AfterViewChecked {
 
     @ContentChildren(PlexLabelComponent) plexLabels: QueryList<PlexLabelComponent>;
     @ContentChildren(PlexLabelComponent, { read: ElementRef }) plexLabelsElement: QueryList<ElementRef>;
+    @ContentChildren(PlexButtonComponent, { descendants: false }) plexButtons: QueryList<PlexButtonComponent>;
 
     get cssDirection() {
         return this.direction === 'row' ? 'direction-row' : 'direction-column';
+    }
+
+    hasButton() {
+        return this.plexButtons.length > 0;
     }
 
     constructor(private render: Renderer2) {
@@ -50,7 +62,7 @@ export class PlexDetailComponent implements AfterViewChecked {
             if (label.subtitulo.length > 28) {
                 this.render.setStyle(native.nativeElement, 'grid-column-end', 'span 2');
             } else {
-                this.render.setStyle(native.nativeElement, 'grid-column-end', 'unset');
+                this.render.setStyle(native.nativeElement, 'grid-column-end', 'span 1');
             }
         });
     }
