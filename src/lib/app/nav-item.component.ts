9244a8a4fa78607dcb3f8ba08e225b0901ef6e5d
ma7payne <matich04@gmail.com>
2025-07-01 13:29:20 -0300

    feat(plex): utiliza material menu para plex-help

--- src/lib/app/nav-item.component.ts
+++ src/lib/app/nav-item.component.ts
@@ -1,27 +1,39 @@
-import { Component, Input, HostBinding, HostListener, ViewChild } from '@angular/core';
+import { Component, Input, HostBinding, HostListener, ViewChild, ElementRef, AfterViewInit, ContentChild } from '@angular/core';
 import { PlexHelpComponent } from '../help/help.component';
+import { PlexIconComponent } from '../icon/icon.component';
 
 @Component({
     // tslint:disable-next-line: component-selector
     selector: 'div[nav-item]',
     template: `
-        <ng-content select="plex-icon"></ng-content>
-        <plex-help class="no-icon" [icon]="null" (close)="onClose()">
+        <plex-help class="no-icon" btnSize="lg" btnType="link" size="lg" (close)="onClose()">
             <div info>
-                <ng-content></ng-content>
+                <ng-content select=":not(plex-icon)"></ng-content>
             </div>
         </plex-help>
 
 
     `,
 })
-export class NavItemComponent {
+export class NavItemComponent implements AfterViewInit {
 
     @HostBinding('class') classList = 'action hidden-sm-down';
 
     @HostBinding('class.hover') opened = false;
 
     @ViewChild(PlexHelpComponent) plexHelp;
+
+    @ContentChild(PlexIconComponent) plexIcon: PlexIconComponent;
+
+    ngAfterViewInit() {
+        console.log(this.plexHelp, this.plexIcon);
+
+        if (this.plexIcon) {
+            const iconName = this.plexIcon.name;
+
+            this.plexHelp.icon = iconName;
+        }
+    }
 
     @HostListener('click', ['event'])
     click() {
@@ -35,5 +47,4 @@ export class NavItemComponent {
     onClose() {
         this.opened = false;
     }
-
 }
